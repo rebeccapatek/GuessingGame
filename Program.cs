971f9ac1bfd79748d858be00ff5283046b49c4de
rebeccapatek <rebeccapatek@gmail.com>
2020-02-27 15:56:17 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GuessingGame {
+    class Program {
+        static void Main (string[] args) {
+            Random rand = new Random ();
+            int secretNumber = rand.Next (1, 11);
+            for (int i = 0; i < 3; i++) {
+
+                Console.WriteLine ("Guess a number from 1 to 10");
+                string userGuess = Console.ReadLine ();
+                int userGuessInt = int.Parse (userGuess);
+                if (userGuessInt == secretNumber) {
+                    Console.WriteLine ($"You guessed it! It was {secretNumber}");
+                    return;
+                } else if (userGuessInt > secretNumber && i != 2) {
+                    Console.WriteLine ("Nope, try sometime lower");
+                } else if (userGuessInt < secretNumber && i != 2) {
+                    Console.WriteLine ("Try Something higher");
+                } else if (userGuessInt != secretNumber && i == 2) {
+                    Console.WriteLine ($"Game Over, it was {secretNumber}");
+                }
+            }
+        }
+    }
+}
